f03bef0e23aa52a1eb0d072276631f29a417203f
Alexander Sukhanov <aosukhanov@gmail.com>
2021-10-12 01:29:15 +0400

    [/] исправил верстку [+] Добавил отображение отложенных и купленных книг в заголовке

---
+++ MyBookShopApp/src/main/java/com/example/mybookshopapp/controllers/ModelAttributeController.java
@@ -0,0 +1,49 @@
+package com.example.mybookshopapp.controllers;
+
+import com.example.mybookshopapp.data.book.Book;
+import com.example.mybookshopapp.repositories.BookRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.CookieValue;
+import org.springframework.web.bind.annotation.ModelAttribute;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@ControllerAdvice
+public class ModelAttributeController {
+
+    BookRepository bookRepository;
+
+    @Autowired
+    public ModelAttributeController(BookRepository bookRepository) {
+        this.bookRepository = bookRepository;
+    }
+
+    @ModelAttribute(name = "bookInPostponedAmount")
+    public Integer bookInPostponedAmount(@CookieValue(value = "postponedContents", required = false) String postponedContents) {
+        if (postponedContents == null || postponedContents.isEmpty()) {
+            return 0;
+        } else {
+            postponedContents = postponedContents.startsWith("/") ? postponedContents.substring(1) : postponedContents;
+            postponedContents = postponedContents.endsWith("/") ? postponedContents.substring(0, postponedContents.length() - 1) : postponedContents;
+            String[] cookieSlug = postponedContents.split("/");
+            List<Book> booksFromCookieSlugs = bookRepository.findBooksBySlugIn(cookieSlug);
+            return booksFromCookieSlugs.size();
+        }
+    }
+
+    @ModelAttribute(name = "bookInCartAmount")
+    public Integer bookInCartAmount(@CookieValue(value = "cartContents", required = false) String cartContents) {
+        if (cartContents == null || cartContents.isEmpty()) {
+            return 0;
+        } else {
+            cartContents = cartContents.startsWith("/") ? cartContents.substring(1) : cartContents;
+            cartContents = cartContents.endsWith("/") ? cartContents.substring(0, cartContents.length() - 1) : cartContents;
+            String[] cookieSlug = cartContents.split("/");
+            List<Book> booksFromCookieSlugs = bookRepository.findBooksBySlugIn(cookieSlug);
+            return booksFromCookieSlugs.size();
+        }
+    }
+}
